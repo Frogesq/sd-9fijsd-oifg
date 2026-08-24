29ccdaaf1987504b66eea968c449f4c8b63c9cb3
Холли <gugurut@inbox.ru>
2026-08-24 21:30:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8116455084:AAE4IeSWFTnacFnf9xjKX7v856HJJVaH-bA")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 # For Telegram Stars (XTR), provider token is usually empty or not required in the same way.
 # We keep it allow-empty for XTR.
 PROVIDER_TOKEN = os.getenv("PROVIDER_TOKEN", "") 
